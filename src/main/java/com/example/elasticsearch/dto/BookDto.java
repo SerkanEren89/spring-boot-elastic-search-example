c38d42ecf09a22e75117c95bca8ef1f479ad5a7c
serkaneren <serkaneren89@gmail.com>
2019-01-08 18:23:23 +0300

    elastic query is added

--- src/main/java/com/example/elasticsearch/dto/BookDto.java
+++ src/main/java/com/example/elasticsearch/dto/BookDto.java
@@ -1,9 +1,5 @@
 package com.example.elasticsearch.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 import java.util.Date;
 
@@ -12,9 +8,6 @@ import java.util.Date;
  *
  * @author eren
  */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class BookDto implements Serializable {
 
     private String isbn;
@@ -34,4 +27,91 @@ public class BookDto implements Serializable {
     private String description;
 
     private String website;
+
+    public BookDto() {
+    }
+
+    public BookDto(String isbn, String title, String subtitle, String author, Date published, String publisher, int pages, String description, String website) {
+        this.isbn = isbn;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.author = author;
+        this.published = published;
+        this.publisher = publisher;
+        this.pages = pages;
+        this.description = description;
+        this.website = website;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublished() {
+        return published;
+    }
+
+    public void setPublished(Date published) {
+        this.published = published;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
 }
