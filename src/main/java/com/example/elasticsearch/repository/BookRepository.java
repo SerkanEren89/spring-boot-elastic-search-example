c4a9006d5a5c963425cbd3c152ddb560f6f67591
eren <serkan.eren@adesso.com.tr>
2019-01-08 09:30:00 +0300

    initial commit

--- src/main/java/com/example/elasticsearch/repository/BookRepository.java
+++ src/main/java/com/example/elasticsearch/repository/BookRepository.java
@@ -3,8 +3,6 @@ package com.example.elasticsearch.repository;
 import com.example.elasticsearch.model.Book;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-import java.util.List;
-
 /**
  * Elastic search book repository
  *
